810ec3946453d77c87c9f6b4e7653cec2d879578
root <root@DESKTOP-DPQ6S79>
2024-10-16 01:45:34 -0400

    Adding navigation menu, still a work in progress

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,72 @@
       <RouterLink to="/">Ena Stewart</RouterLink>
       <div class="nav-right">
         <RouterLink to="/">
-          <BrushIcon/>
-          Gallery
+          <div class="flex align-center" style="gap: 0.2rem">
+            <BrushIcon style="width: 2rem; height: 2rem; fill: black;" />
+            <span class="nav-text">Gallery</span>
+          </div>
         </RouterLink> |
-        <RouterLink to="/archive">Archive</RouterLink> |
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/archive">
+          <div class="flex align-center" style="gap: 0.2rem">
+            <PhotoAlbumIcon style="width: 2rem; height: 2rem; fill: black;" />
+            <span class="nav-text">Archive</span>
+          </div>
+        </RouterLink> |
+        <RouterLink to="/about">
+          <div class="flex align-center" style="gap: 0.2rem">
+            <PersonIcon style="width: 2rem; height: 2rem; fill: black;" />
+            <span class="nav-text">About</span>
+          </div>
+        </RouterLink>
+      </div>
+      <div
+        class="nav-right-mobile"
+        style="z-index: 1001"
+        @click="showMenu = !showMenu"
+      >
+        <MenuIcon style="width: 2rem; height: 2rem; fill: black;" />
       </div>
     </nav>
     <main>
       <RouterView />
+      <div
+        v-if="showMenu"
+        class="menu-overlay"
+      >
+        <div class="menu" :class="{ 'menu-active': showMenu }">
+          <RouterLink to="/">
+            <div class="flex align-center" style="gap: 0.2rem">
+              <BrushIcon style="width: 2rem; height: 2rem; fill: black;" />
+              <span class="nav-text">Gallery</span>
+            </div>
+          </RouterLink>
+          <RouterLink to="/archive">
+            <div class="flex align-center" style="gap: 0.2rem">
+              <PhotoAlbumIcon style="width: 2rem; height: 2rem; fill: black;" />
+              <span class="nav-text">Archive</span>
+            </div>
+          </RouterLink>
+          <RouterLink to="/about">
+            <div class="flex align-center" style="gap: 0.2rem">
+              <PersonIcon style="width: 2rem; height: 2rem; fill: black;" />
+              <span class="nav-text">About</span>
+            </div>
+          </RouterLink>
+        </div>
+      </div>
     </main>
   </div>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router';
-import { BrushIcon } from '@components/BrushIcon.vue'
+import BrushIcon from '@components/BrushIcon.vue'
+import PhotoAlbumIcon from '@components/PhotoAlbumIcon.vue';
+import PersonIcon from '@components/PersonIcon.vue'
+import MenuIcon from '@components/MenuIcon.vue'
+import { ref } from 'vue';
+
+const showMenu = ref(false)
 
 </script>
 
@@ -47,11 +97,54 @@ main {
   gap: 1rem;
 }
 
+.nav-right-mobile {
+  display: none;
+}
+
 .footer {
   display: flex;
   justify-content: end;
   background-color: white;
   padding: 2rem;
+}
+
+.menu-overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 0px;
+  height: 100%;
+  background-color: white;
+  transition: width 0.3s ease;
+  margin-left: auto;
+}
+
+.menu-active {
+  width: 400px;
+  transition: width 0.3s ease;
+}
+
+@media screen and (max-width: 1000px) {
+  .nav-right-mobile {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav-right {
+    display: none;
+  }
 }
 
 </style>

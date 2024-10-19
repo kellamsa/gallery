62d706529af7519d59ce20affb3536ba1a009389
root <root@DESKTOP-DPQ6S79>
2024-10-19 00:11:31 -0400

    Finalizing the about section a bit more

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -1,19 +1,62 @@
 <template>
   <div class="about">
-    <p>
-      <PersonIcon style="display: inline-block; width: 15rem; height: 15rem;"/>
-      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.     Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-    </p>
+    <div class="about__main">
+      <PersonIcon class="about__image" />
+      <p class="about__text">
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
+      </p>
+    </div>
+    <div class="about__contact">
+      <div style="font-size: 1.5rem;">
+        
+      </div>
+      <a href="https://www.instagram.com/enastewartart" target="_blank" class="flex align-center" style="gap: 0.5rem">
+        <InstagramIcon style="fill: black;"/>
+        <span>Visit me on <span style="font-weight: 500; text-decoration:underline">Instagram</span></span>
+      </a>
+      <div>
+        or email me directly 
+      </div>
+      <div class="flex align-center" style="gap: 0.5rem; font-weight: 500;">
+        <MailIcon style="fill: red;"/>
+        kellamsandrew@gmail.com
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import PersonIcon from '@components/PersonIcon.vue'
+import MailIcon from '@components/MailIcon.vue'
+import InstagramIcon from '@components/InstagramIcon.vue'
 </script>
 
 
 <style lang="scss" scoped>
-.about {
+.about__main, .about__contact {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding: 1rem;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.about__contact {
+  gap: 0.75rem; 
+  font-weight: 500;
+}
+
+.about__text {
+  text-align: center;
+}
+
+.about__image {
+  margin: 1rem;
+  width: 12rem;
+  height: 12rem;
+  border-radius: 50%;
+  border: solid 2px black;
+  fill: black;
 }
 </style>
